b9ea86ee9d6ad9944385000e6265999c7a4ae2ff
fjesser <felixjonas.esser@googlemail.com>
2022-02-01 00:51:24 -0500

    Add init-org file

---
+++ init-org.el
@@ -0,0 +1,8 @@
+;;; init-org.el --- Org Configuration File
+;; This file is loaded from within the emacs init file "~/.emacs.d/init.el"
+;; Attention: Some org configruations are configured in the init.el file
+;; because it was not possible otherwise. 
+;;  = new page; jump with C-x ] to next page and C-x [ to previous page
+;; better: use outline-minor-mode (L1: ;;; L2: ;;;; etc.)
+;; my prefix: C-c C-u, use with C-n, C-p etc., C-<TAB> to fold
+
