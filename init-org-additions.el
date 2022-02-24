0f91591a811a3f04a5a07469778ef6903b50e849
fjesser <felixjonas.esser@googlemail.com>
2022-02-23 23:44:32 -0500

    Add org-roam keybindings

--- init-org-additions.el
+++ init-org-additions.el
@@ -26,3 +26,28 @@
 ;; Change display formatting when searching for org-roam-nodes
 ;; more space for tags
 (setq org-roam-node-display-template "${title:125} ${tags:75}")
+
+
+
+;;;;; --------- Org-Roam keybindings
+;; Org mode ID function
+(global-set-key (kbd "C-c M-i") 'org-id-get-create)                ; actually not org-roam
+;; All org-roam functions have the prefix C-ö
+(global-set-key (kbd "C-ö f") 'org-roam-node-find)                 ; find/create org-roam file
+(global-set-key (kbd "C-ö i") 'org-roam-node-insert)               ; insert link
+(global-set-key (kbd "C-ö c") 'org-roam-capture)                   ; create org-roam file
+(global-set-key (kbd "C-ö b") 'org-roam-buffer-toggle)             ; toggle roam buffer
+(global-set-key (kbd "C-ö d") 'org-roam-buffer-display-dedicated)  ; choose roam buffer
+(global-set-key (kbd "C-ö a") '(lambda () ; C-ö a = add alias; with C-u remove alias
+				 (interactive)			    
+				 (if (equal current-prefix-arg nil)            ; if no C-u
+				     (call-interactively 'org-roam-alias-add)  ; add alias
+				   (call-interactively 'org-roam-alias-remove) ; else remove alias
+				     )))
+(global-set-key (kbd "C-ö r") '(lambda () ; C-ö r = add ref; with C-u remove ref
+				 (interactive)			    
+				 (if (equal current-prefix-arg nil)          ; if no C-u
+				     (call-interactively 'org-roam-ref-add)  ; add ref
+				   (call-interactively 'org-roam-ref-remove) ; else remove ref
+				     )))
+(global-set-key (kbd "C-ö g") 'org-roam-graph)
